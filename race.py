17d83f603ca76cfa9be8e2bada68362bff7c7b56
erinszabo <55733370+erinszabo@users.noreply.github.com>
2021-09-24 15:16:54 -0700

    tested driver class and finished get_info

--- race.py
+++ race.py
@@ -1,3 +1,4 @@
+
 # Driver class with traits name, number and line
 class Driver:
     def __init__(self, name, number, line):
@@ -23,11 +24,51 @@ class Driver:
 
 
 # get info from file to create driver objects
+# returns an array of driver objects
 def get_info():
-    # file_in = raw_input("driver file: ")
     file = open("drivers.txt", "r")
+    drivers = [] # to store driver objects
+    
+    # populate drivers array with driver objects using file info 
     for i in range(19): # 20 drivers
-        driver = file.readline()
-        # ..... continue later
-        # print(driver, end = '') # no new line
+        line = file.readline()  # the whole line is the "line"
+        # split the line by the spaces
+        parts = line.split()
+        name = str(parts[0]) + " " + str(parts[1])  # first 2 words are the "name" of the driver
+        num = parts[2]  # 3rd is the "number"
+
+        ### TEST ###
+        # print(f" line = {line} name = {name} \n num = {num} \n ")
+
+        #create a driver object
+        d = Driver(name, num, line)
+        drivers.append(d)
+
+    ### TEST ###
+    # print(f"driver[0] name = {drivers[0].get_name()}")
+    # print(f"driver[0] num = {drivers[0].get_num()}")
+    # print(f"driver[0] line = {drivers[0].get_line()}")
+
+
+
+    return drivers
+
+# sorts drivers array by name
+def name_sort():
     return
+
+# sorts drivers array by number
+def num_sort():
+    return
+
+# asks user if drivers should be sorted by name or number, 
+# then calls the required sort
+def choice():
+    # prompt user
+    return  
+
+# prints the sorted list of drivers
+def display(d_array):
+    # print(driver, end = '') # no new line
+
+    return 
