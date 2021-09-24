c07af9782d43c2410bb229b68a2b8409a4e22cb8
erinszabo <55733370+erinszabo@users.noreply.github.com>
2021-09-24 16:55:12 -0700

    declutter tests, Project complete.

--- main.py
+++ main.py
@@ -5,11 +5,8 @@ import race as r
 if __name__ == '__main__':
     drivers = r.get_info()
     
-    r.display(drivers)  # FOR TESTING, DELETE LATER
-
     drivers = r.name_sort(drivers)  # sort by name
     r.display(drivers) 
 
     drivers = r.num_sort(drivers)  # sort by number
     r.display(drivers) 
-
--- race.py
+++ race.py
@@ -16,11 +16,10 @@ class Driver:
     def get_line(self):
         return self.li
 
-    # setter for line, so I can add " YEET!" if name == Lewis Hamilton
+    # setter for line, so I can add " YEET!" if driver name is Lewis Hamilton
     def check(self):
-        if self.na == "Lewis Hamilton":
+        if self.na == "Hamilton":
             self.li = self.li + " YEET!"
-
 
 
 # get info from file to create driver objects
@@ -34,28 +33,15 @@ def get_info():
         line = file.readline()  # the whole line is the "line"
         # split the line by the spaces
         parts = line.split()
-        
-        ### name = str(parts[0]) + " " + str(parts[1])  # first 2 words are the "name" of the driver
-        ### ^ DELETE ME ^
 
         # easier to use just the last name
         name = str(parts[1]) # second word is last name
         
         num = parts[2]  # 3rd is the "number"
 
-        ### TEST ###
-        # print(f" line = {line} name = {name} \n num = {num} \n ")
-
         #create a driver object
         d = Driver(name, num, line)
         drivers.append(d)
-
-    ### TEST ###
-    # print(f"driver[0] name = {drivers[0].get_name()}")
-    # print(f"driver[0] num = {drivers[0].get_num()}")
-    # print(f"driver[0] line = {drivers[0].get_line()}")
-
-
 
     return drivers
 
@@ -69,15 +55,13 @@ def num_sort(arr):
     arr.sort(key=lambda x: int(x.nu))
     return arr
 
-
 # prints the sorted list of drivers
 def display(d_array):
     print("")
     print("2021 F1 Drivers")
     print("===============")
+
     for driver in d_array:
         print(driver.get_line(), end = '') # no new line
-        # print(driver.get_num()) 
-        # print(driver.get_name())
 
     return 
